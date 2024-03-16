8544d51e904c3b1b14d1bb87377f8f2480c952a8
Neelzee <nilsien2001@gmail.com>
2024-03-16 18:02:52 +0100

    refactored whisky methods

--- src/models/whisky.rs
+++ src/models/whisky.rs
@@ -13,3 +13,40 @@ pub struct Whiskey {
     pub title: String,
     pub volume: f32,
 }
+
+impl Whiskey {
+    pub fn new(
+        id: u32,
+        img: String,
+        percentage: u32,
+        price: f32,
+        rating: f32,
+        summary: String,
+        title: String,
+        volume: f32,
+    ) -> Self {
+        Whiskey {
+            id,
+            img,
+            percentage,
+            price,
+            rating,
+            summary,
+            title,
+            volume,
+        }
+    }
+
+    pub fn from_raw(mut raw: RawWhisky) -> Self {
+        Whiskey {
+            id: 0,
+            img: raw.images.pop().expect("Expected an image").url,
+            percentage: raw.alcohol.value as u32,
+            price: raw.price.value,
+            rating: 0f32,
+            summary: raw.summary,
+            title: raw.name,
+            volume: raw.volume.value,
+        }
+    }
+}
