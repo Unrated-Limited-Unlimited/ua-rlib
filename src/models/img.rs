a98af18c97e70b080e58740d7729e6e63aa9f4ce
Neelzee <nilsien2001@gmail.com>
2024-03-16 20:47:56 +0100

    added img-type field to img

--- src/models/img.rs
+++ src/models/img.rs
@@ -4,4 +4,13 @@ use serde::{Deserialize, Serialize};
 pub struct Img {
     alt: String,
     img: Vec<u8>,
+    img_type: ImgType,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ImgType {
+    #[serde(rename = "whiskyImage")]
+    WhiskyImage,
+    #[serde(rename = "profileImage")]
+    ProfileImage,
 }
