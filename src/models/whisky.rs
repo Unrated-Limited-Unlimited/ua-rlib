0a8d8012f9fbbd6505cd50e9a201051b862dea2f
Neelzee <nilsien2001@gmail.com>
2024-03-19 22:09:08 +0100

    implemented whisky from value

--- src/models/whisky.rs
+++ src/models/whisky.rs
@@ -1,6 +1,7 @@
-use serde::{Deserialize, Serialize};
-
 use super::raw_whisky::RawWhisky;
+use eyre::{Context, OptionExt, Result};
+use serde::{Deserialize, Serialize};
+use serde_json::{self, Value};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Whiskey {
@@ -48,5 +49,30 @@ impl Whiskey {
             title: raw.name,
             volume: raw.volume.value,
         }
+    }
+
+    pub fn from_value(value: Value) -> Result<Self> {
+        Ok(Whiskey {
+            id: 0,
+            img: value["images"]
+                .as_array()
+                .ok_or_eyre(format!("Failed getting `images` from `{value:?}`"))?[0]["url"]
+                .to_string(),
+            percentage: value["alcohol"]["value"]
+                .to_string()
+                .parse::<u32>()
+                .wrap_err(format!("Failed getting `alcohol.value` from `{value:?}`"))?,
+            price: value["price"]["value"]
+                .to_string()
+                .parse::<f32>()
+                .wrap_err(format!("Failed getting `price.value` from `{value:?}`"))?,
+            rating: 0f32,
+            summary: value["summary"].to_string(),
+            title: value["name"].to_string(),
+            volume: value["volume"]["value"]
+                .to_string()
+                .parse::<f32>()
+                .wrap_err(format!("Failed getting `price.value` from `{value:?}`"))?,
+        })
     }
 }
