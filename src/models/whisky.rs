94e3ae68f2a0ce905bbe173359e131405af25216
Neelzee <nilsien2001@gmail.com>
2024-03-19 22:18:40 +0100

    corrected usage of serde_json

--- src/models/whisky.rs
+++ src/models/whisky.rs
@@ -5,26 +5,26 @@ use serde_json::{self, Value};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Whiskey {
-    pub id: u32,
+    pub id: u64,
     pub img: String,
-    pub percentage: u32,
-    pub price: f32,
-    pub rating: f32,
+    pub percentage: u64,
+    pub price: f64,
+    pub rating: f64,
     pub summary: String,
     pub title: String,
-    pub volume: f32,
+    pub volume: f64,
 }
 
 impl Whiskey {
     pub fn new(
-        id: u32,
+        id: u64,
         img: String,
-        percentage: u32,
-        price: f32,
-        rating: f32,
+        percentage: u64,
+        price: f64,
+        rating: f64,
         summary: String,
         title: String,
-        volume: f32,
+        volume: f64,
     ) -> Self {
         Whiskey {
             id,
@@ -42,12 +42,12 @@ impl Whiskey {
         Whiskey {
             id: 0,
             img: raw.images.pop().expect("Expected an image").url,
-            percentage: raw.alcohol.value as u32,
-            price: raw.price.value,
-            rating: 0f32,
+            percentage: raw.alcohol.value as u64,
+            price: raw.price.value as f64,
+            rating: 0f64,
             summary: raw.summary,
             title: raw.name,
-            volume: raw.volume.value,
+            volume: raw.volume.value as f64,
         }
     }
 
@@ -59,20 +59,20 @@ impl Whiskey {
                 .ok_or_eyre(format!("Failed getting `images` from `{value:?}`"))?[0]["url"]
                 .to_string(),
             percentage: value["alcohol"]["value"]
-                .to_string()
-                .parse::<u32>()
-                .wrap_err(format!("Failed getting `alcohol.value` from `{value:?}`"))?,
+                .as_number()
+                .and_then(|e| e.as_u64())
+                .ok_or_eyre(format!("Failed getting `alcohol.value` from `{value:?}`"))?,
             price: value["price"]["value"]
-                .to_string()
-                .parse::<f32>()
-                .wrap_err(format!("Failed getting `price.value` from `{value:?}`"))?,
-            rating: 0f32,
+                .as_number()
+                .and_then(|e| e.as_f64())
+                .ok_or_eyre(format!("Failed getting `price.value` from `{value:?}`"))?,
+            rating: 0f64,
             summary: value["summary"].to_string(),
             title: value["name"].to_string(),
             volume: value["volume"]["value"]
-                .to_string()
-                .parse::<f32>()
-                .wrap_err(format!("Failed getting `price.value` from `{value:?}`"))?,
+                .as_number()
+                .and_then(|e| e.as_f64())
+                .ok_or_eyre(format!("Failed getting `price.value` from `{value:?}`"))?,
         })
     }
 }
