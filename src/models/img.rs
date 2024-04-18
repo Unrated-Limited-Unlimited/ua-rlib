b657186f9d23aabd23b20cf8dc93e1d22082e60b
Neelzee <nilsien2001@gmail.com>
2024-04-18 19:28:22 +0200

    added imgwrapper and imgresponse

--- src/models/img.rs
+++ src/models/img.rs
@@ -1,5 +1,7 @@
+use rocket::Responder;
 use serde::{Deserialize, Serialize};
 
+#[deprecated(since = "1.3.2", note = "Not used")]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Img {
     alt: String,
@@ -8,10 +10,25 @@ pub struct Img {
     id: String,
 }
 
+#[deprecated(since = "1.3.2", note = "Not used")]
 #[derive(Debug, Serialize, Deserialize)]
 pub enum ImgType {
     #[serde(rename = "whiskyImage")]
     WhiskyImage,
     #[serde(rename = "profileImage")]
     ProfileImage,
+}
+
+/// Image Response
+///
+/// Sets response status and content-type header to image/jpeg
+#[derive(Responder)]
+#[response(status = 200, content_type = "image/jpeg")]
+pub struct ImgResponse(Vec<u8>);
+
+/// Image wrapper used for storing it in the database
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ImgWrapper {
+    pub img: Vec<u8>,
+    pub iid: String,
 }
